1fa93e4718bec47834ec9880fe986c49020e3ace
Alexandre Lecoutre <alecoutr@k1r4p13.42mulhouse.fr>
2022-11-05 23:24:10 +0100

    Fixes

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,50 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alecoutr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: alecoutr <alecoutr@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/18 02:58:11 by alecoutr          #+#    #+#             */
-/*   Updated: 2022/07/18 17:34:36 by alecoutr         ###   ########.fr       */
+/*   Created: 2022/11/05 23:05:07 by alecoutr          #+#    #+#             */
+/*   Updated: 2022/11/05 23:22:58 by alecoutr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	temp;
 	size_t	length;
-	
+
 	if (*needle == '\0')
 		return ((char *)haystack);
 	i = 0;
-	length = ft_strlen(needle);
-	while (i < len)
+	length = 0;
+	while (needle[length])
+		length++;
+	while (i < len && *haystack)
 	{
 		temp = 0;
 		if (*haystack == needle[temp])
 		{
-			while (needle[temp] && needle[temp] == haystack[temp])
+			while ((needle[temp] && needle[temp] == haystack[temp])
+				&& (i + temp < len))
 				temp++;
 			if (temp == length)
 				return ((char *)haystack);
-			haystack++;
 		}
+		haystack++;
 		i++;
 	}
-	return (0);
-}
-
-int main(void)
-{
-	char big[] = "abcdefkjdoawjodaw";
-	char small[] = "cd";
-
-	printf("%s\n", ft_strnstr(big, small, 5));
-	
 	return (0);
 }
